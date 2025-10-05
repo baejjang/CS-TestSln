8fa54b727572cd5c562fdda1cdb81669385d686d
EunGoo <61935751+baejjang@users.noreply.github.com>
2025-10-05 21:34:22 +0900

    EventDelegate
    EventDelegate

---
+++ Exam_EventDelegate/Exam_EventDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApplication42
+{
+    public delegate void MyEventHandler(string message);
+
+    class Publisher
+    {
+        public event MyEventHandler Active;
+
+        public void DoActive(int number)
+        {
+            if (number % 10 == 0)
+                Active("Active!" + number);
+            else
+                Console.WriteLine(number);
+        }
+    }
+
+    class Subscriber
+    {
+        static public void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher();
+            publisher.Active += new MyEventHandler(MyHandler);
+
+            for (int i = 1; i < 50; i++)
+                publisher.DoActive(i);
+
+            Console.ReadLine();
+        }
+    }
+}
